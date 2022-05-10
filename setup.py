f5f47f36cda562697a399ed75bed29902d3cfdfe
Gregor von Laszewski <laszewski@gmail.com>
2022-05-10 09:57:59 -0400

    move documentation n proper place

--- setup.py
+++ setup.py
@@ -69,8 +69,9 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+
     ],
     install_requires=requiers,
     tests_require=[
